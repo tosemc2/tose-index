27cb2ce2d73e38428904d24422379800735801df
tosemc2 <natsusao67@gmail.com>
2026-01-01 15:55:37 +0900

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,10 @@
-document.getElementById("contactForm").addEventListener("submit", function(e){
-  e.preventDefault(); // ページ遷移を防ぐ
-
-  const data = {
-    category: document.getElementById("category").value,
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value
-  };
-  const params = new URLSearchParams(data);
-  fetch("https://script.google.com/macros/s/AKfycbxvAdADEQ3CRmzU7iYb--j_fBw7YXErJsg-fL9PkF3BRyJ2qzsMu4sCYOsC5ITeJf1P/exec", { // ← コピーしたWebアプリURL
-    method: "POST",
-    body: params
-  });
-}
+    const form = document.getElementById("contactForm");
+    form.addEventListener("submit", () => {
+      const last = localStorage.getItem("lastSubmit");
+      if (last && Date.now() - last < 60000) {
+        alert("送信間隔を空けてください。");
+        event.preventDefault();
+        return;
+      }
+      localStorage.setItem("lastSubmit", Date.now());
+    });
