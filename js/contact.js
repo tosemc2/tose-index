2020aac1464093b2a69ef91c1ece6651f7773316
tosemc2 <natsusao67@gmail.com>
2026-02-05 00:02:06 +0900

    Add files via upload

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,23 @@
-    const form = document.getElementById("contactForm");
-    form.addEventListener("submit", () => {
+console.log(
+  document.querySelectorAll('.js-smooth[href^="#"]').length
+);
+
+
+(() => {
+  // フォーム送信制御
+  const form = document.getElementById("contact-form");
+
+  if (form) {
+    form.addEventListener("submit", (event) => {
       const last = localStorage.getItem("lastSubmit");
+
       if (last && Date.now() - last < 60000) {
         alert("送信間隔を空けてください。");
         event.preventDefault();
         return;
       }
+
       localStorage.setItem("lastSubmit", Date.now());
     });
+  }
+});
